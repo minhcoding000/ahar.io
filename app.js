67898647214f063992733230cc55479918f2d226
Minh Hoang <minhcoding000@gmail.com>
2018-01-26 23:01:37 +0000

    Hello2

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
-
+var x = 250;
+var y = 250;
 
 function drawCircle() {
-context.beginPath();
-context.arc(250,250,40,0,2*3.14159);
-context.fillStyle = "cyan";
-context.fill();
+    context.beginPath();
+    context.arc(x,y,40,0,2*3.14159);
+    context.fillStyle = "cyan";
+    context.fill();
 }
 
+function clearCanvas() {
+    context.beginPath();
+    context.rect(0,0,500,500);
+    context.fillStyle = "white";
+    context.fill();
+}
+
+function mouseMoved(mouse) {
+    x = mouse.clientX;
+    y = mouse.clientY;
+    clearCanvas();
+    drawCircle();
+}
+
+canvas.addEventListener("mousemove", mouseMoved);
 drawCircle();
